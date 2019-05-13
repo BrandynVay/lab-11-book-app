635d73e4cc17107e27a235b314cb49b19794542f
KlNGU <brandynvay@yahoo.com>
2019-05-12 19:15:06 -0700

    update working on a bug right now

--- data/books.sql
+++ data/books.sql
@@ -2,12 +2,15 @@
 
 DROP TABLE IF EXISTS books;
 
-CREATE TABLE books (
+CREATE TABLE IF NOT EXISTS books (
   id SERIAL PRIMARY KEY,
   title VARCHAR(255),
   author VARCHAR(255),
   isbn VARCHAR(255),
-  description VARCHAR(255),
+  description TEXT,
   bookshelf VARCHAR(255),
   image_url VARCHAR(255)
 );
+
+INSERT INTO books (author, title, isbn, description, bookshelf, image_url)
+VALUES ('brandyn', 'book', 1234567891011,'i am the discription', 'fiction', 'https://i.imgur.com/J5LVHEL.jpg');
